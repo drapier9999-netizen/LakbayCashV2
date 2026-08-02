72e6a3bbc8013509abd0bd838876ad1b2abac2b9
drapier9999-netizen <drapier9999@gmail.com>
2026-08-02 21:28:24 +0800

    Delete htdocs directory

--- htdocs/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/auth.php';
-require_admin();
-
-// Logout
-$_SESSION = array();
-session_destroy();
-header('Location: ' . BASE_URL . '/admin/login.php');
-exit;
--- htdocs/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-$_SESSION = array();
-session_destroy();
-redirect('index.php');
